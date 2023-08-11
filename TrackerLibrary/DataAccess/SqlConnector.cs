45f168cc9a7f61e9254870b1debfee2627bb05a4
KeremDUZENLI <krmdznl97@gmail.com>
2023-08-11 19:34:17 -0300

    build: connection string with sql-server created

---
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibrary.Models;
+
+//@PlaceNumber int
+//@PlaceName nvarchar(50)
+//@PrizeAmount money
+//@PrizePercentage float
+//@id int = 0 output
+
+namespace TrackerLibrary.DataAccess
+{
+    public class SqlConnector : IDataConnection
+    {
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            var conn = GlobalConfig.CnnString("Tournaments");
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.CnnString("Tournaments")))
+            {
+                var p = new DynamicParameters();
+                p.Add("@PlaceNumber", model.PlaceNumber);
+                p.Add("@PlaceName", model.PlaceName);
+                p.Add("@PrizeAmount", model.PrizeAmount);
+                p.Add("@PrizePercentage", model.PrizePercentage);
+                p.Add("@id", dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+                connection.Execute("dbo.spPrizes_Insert", p, commandType: CommandType.StoredProcedure);
+                model.Id = p.Get<int>("@id");
+
+                return model;
+            }
+        }
+    }
+}
